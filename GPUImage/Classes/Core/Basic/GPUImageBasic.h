ce24da2766b19f02724b9e4ff82d2c5cee51adca
袁平华 <yuanph@ushareit.com>
2017-06-01 15:01:57 +0800

    add header

---
+++ GPUImage/Classes/Core/Basic/GPUImageBasic.h
@@ -0,0 +1,32 @@
+//
+//  GPUImageBasic.h
+//  Pods
+//
+//  Created by 袁平华 on 2017/6/1.
+//
+//
+
+#ifndef GPUImageBasic_h
+#define GPUImageBasic_h
+
+#import "GPUImageContext.h"
+#import "GPUImageFilter.h"
+#import "GPUImageFramebuffer.h"
+#import "GPUImageFramebufferCache.h"
+#import "GPUImageMovieWriter.h"
+#import "GPUImageRawDataOutput.h"
+#import "GPUImageTextureOutput.h"
+#import "GPUImageView.h"
+#import "GPUImageFilterPipeline.h"
+#import "GPUImageColorConversion.h"
+#import "GPUImageMovie.h"
+#import "GPUImageOutput.h"
+#import "GPUImagePicture+TextureSubimage.h"
+#import "GPUImagePicture.h"
+#import "GPUImageRawDataInput.h"
+#import "GPUImageStillCamera.h"
+#import "GPUImageTextureInput.h"
+#import "GPUImageUIElement.h"
+#import "GPUImageVideoCamera.h"
+#import "GLProgram.h"
+#endif /* GPUImageBasic_h */
